aea1fc59d5633c439141e2e8e3e1f1fe6a9d998a
Jørgen Galdal <jorgen.4@live.no>
2024-09-28 17:01:13 +0200

    initial commit
    Started on graph component and fetch from hkdir

---
+++ src/types.ts
@@ -0,0 +1,11 @@
+export interface GradeEntry {
+    year: number;
+    semester: string;
+    studyProgrammeCode: string;
+    studyProgrammeName: string;
+    classCode: string;
+    grade: string;
+    totalCandidates: number;
+    women: number;
+    men: number;
+}
